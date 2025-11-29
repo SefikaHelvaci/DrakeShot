011ec6f89b7b4be5edd9a029d21eab7999571883
Mustafa Eren Şahin <mustafa.eren.sahin@outlook.com>
2025-11-30 01:39:32 +0300

    Esc Menu
    I created the esc menu in the arena scene and made it dontDestroy() this menu opens when you press esc key and closes when you press it again also you can press the resume button to close it as well. I am working on settings and quit button.

---
+++ DrakeShot-main/Drake Shot/Assets/scripts/QuitButtonScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class QuitButtonScript : MonoBehaviour {
+    
+    //Should work in .exe game.
+    public void QuitGame() {
+        
+        Application.Quit();
+        
+    }
+    
+}
