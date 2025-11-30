67054d98d6744911eb09a588931a9873d9f6159c
SefikaH <hmmmakessense@gmail.com>
2025-11-30 15:54:18 +0300

    player no longer gets pushed off
    made is so that player no longer gets pushed into the wall halfway by the enemies when it gets squeezed against the wall. kept the old code in comments just in case

--- DrakeShot-main/Drake Shot/Assets/scripts/chaseTarget.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/chaseTarget.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class chaseTarget : MonoBehaviour
 {
+    // commented these out in case this one doesent work
+    //or has bugs down the road
+    //because these work fine other than pushing the player
+    //into the walls
     
-    public GameObject movingObject;  
-    public GameObject targetPosition; 
+    
+    //public GameObject movingObject;  
+    //public GameObject targetPosition; 
+    public Rigidbody2D rb;
+    public Transform target;
     public float speed = 1f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,13 +23,19 @@ public class chaseTarget : MonoBehaviour
     void Update()
     {
         
-        if (targetPosition != null)
+        //if (targetPosition != null)
+        //{
+            //movingObject.transform.position = Vector3.MoveTowards(
+                //movingObject.transform.position,
+                //targetPosition.transform.position,
+                //speed * Time.deltaTime
+            //);
+        //}
+
+        if (target != null)
         {
-            movingObject.transform.position = Vector3.MoveTowards(
-                movingObject.transform.position,
-                targetPosition.transform.position,
-                speed * Time.deltaTime
-            );
+            Vector2 nextPos = Vector2.MoveTowards(rb.position, target.position, speed * Time.deltaTime);
+            rb.MovePosition(nextPos);
         }
         
     }
