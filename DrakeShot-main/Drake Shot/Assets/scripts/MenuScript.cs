c36bef1ad275933c42c4cbdc9b26a305c66c1804
Mustafa Eren Şahin <mustafa.eren.sahin@outlook.com>
2025-11-30 21:55:26 +0300

    Settings Menu
    I created the settings menu and made it accessible from the esc menu.

--- DrakeShot-main/Drake Shot/Assets/scripts/MenuScript.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/MenuScript.cs
@@ -4,6 +4,7 @@ public class MenuScript : MonoBehaviour {
     
     public GameObject escMenu;
     public GameObject skillsMenu;
+    public GameObject settingsMenu;
     private GameObject _openedMenu;
     
     private void Awake() {
@@ -12,6 +13,7 @@ public class MenuScript : MonoBehaviour {
         
         escMenu.SetActive(false);
         skillsMenu.SetActive(false);
+        settingsMenu.SetActive(false);
         
     }
 
@@ -23,56 +25,70 @@ public class MenuScript : MonoBehaviour {
         else if (Input.GetKeyUp(KeyCode.K)) {
             DoMenuOp(skillsMenu);
         }
-
-        void DoMenuOp(GameObject menu) {
-            
-            if (!_openedMenu) {
-                OpenMenu(menu);
-            }
-            else if (_openedMenu == menu) {
-                CloseMenu(menu);
-            }
-            else {
-                OpenAnotherMenu(menu);
-            }
-            
-        }
-        
-        void CloseMenu(GameObject menu) {
-        
-            menu.SetActive(false);
-            _openedMenu = null;
-        
-            Time.timeScale = 1f;
-        
-        }
-        
-        void OpenMenu(GameObject menu) {
-            
-            menu.SetActive(true);
-            _openedMenu = menu;
-            
-            Time.timeScale = 0f;
-            
-        }
-
-        void OpenAnotherMenu(GameObject menu) {
-            
-            _openedMenu.SetActive(false);
-            menu.SetActive(true);
-            _openedMenu = menu;
-
-        }
         
     }
     
-    //This func. is not private or inside Update() because resume button uses it.
+    private void DoMenuOp(GameObject menu) {
+            
+        if (!_openedMenu) {
+            OpenMenu(menu);
+        }
+        else if (_openedMenu == menu) {
+            CloseMenu(menu);
+        }
+        else {
+            OpenAnotherMenu(menu);
+        }
+            
+    }
+    
+    public void CloseMenu(GameObject menu) {
+        
+        menu.SetActive(false);
+        _openedMenu = null;
+        
+        Time.timeScale = 1f;
+        
+    }
+    
+    private void OpenMenu(GameObject menu) {
+            
+        menu.SetActive(true);
+        _openedMenu = menu;
+            
+        Time.timeScale = 0f;
+            
+    }
+    
+    private void OpenAnotherMenu(GameObject menu) {
+            
+        _openedMenu.SetActive(false);
+        menu.SetActive(true);
+        _openedMenu = menu;
+
+    }
+    
+    //This func. is not private because resume button uses it.
     public void CloseEscMenuForResumeButton() {
         
         escMenu.SetActive(false);
         _openedMenu = null;
         
         Time.timeScale = 1f;
+        
+    }
+    
+    //This func. is not private because settings button uses it.
+    public void OpenSettingsMenu() {
+        DoMenuOp(settingsMenu);
+    }
+    
+    //This func. is not private because back button uses it.
+    public void CloseSettingsMenuForBackButton() {
+        
+        settingsMenu.SetActive(false);
+        escMenu.SetActive(true);
+        _openedMenu = escMenu;
         
     }
     
