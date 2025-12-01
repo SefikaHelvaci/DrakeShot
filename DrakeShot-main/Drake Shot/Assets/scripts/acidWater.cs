ea2a658a4f5d56a873d919be5c2e70385201100f
SefikaH <hmmmakessense@gmail.com>
2025-12-02 01:01:19 +0300

    tried to make it water hurts continiously. didint work
    currently hp only goes down when player exists one hitbox and enters the other. will probably have to ask about it.

--- DrakeShot-main/Drake Shot/Assets/scripts/acidWater.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/acidWater.cs
@@ -1,40 +1,57 @@
+using System.Collections;
 using UnityEngine;
 
 public class acidWater : MonoBehaviour
 {
 
     public int damage = 1;
+    private bool dmgable = true;
+
+    public float hitSpeed = 1f;
+    //^^ privatize this one after tests
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            getHit hitScript = col.GetComponent<getHit>();
 
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player") && dmgable)
+        {
+
+            getHit hitScript = col.GetComponent<getHit>();
             if (hitScript != null)
             {
-                hitScript.HP -= damage;
-                Debug.Log("Player got soaked by water. HP now: " + hitScript.HP);
-
-                if (hitScript.HP <= 0)
-                {
-                    Debug.Log("Player died!");
-                    Destroy(col.gameObject);
-                }
+                StartCoroutine(waterDamage(hitScript));
             }
-
-            Destroy(gameObject);
         }
     }
+
+
+    IEnumerator waterDamage(getHit hitScript)
+    {
+        dmgable = false;
+        hitScript.HP -= damage;
+        Debug.Log("Player got soaked by water. HP now: " + hitScript.HP);
+        
+        if(hitScript.HP <= 0){
+            Debug.Log("Player drowned :(");
+            Destroy(hitScript.gameObject);
+            yield break;
+        }
+        yield return new WaitForSeconds(hitSpeed);
+        dmgable = true;
+    }
+
+
+
+
 }
