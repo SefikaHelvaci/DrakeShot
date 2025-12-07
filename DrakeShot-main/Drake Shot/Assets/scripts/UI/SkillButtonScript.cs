62468b3d950873aea3f010d6ae0bdd533b51ec97
Mustafa Eren Şahin <mustafa.eren.sahin@outlook.com>
2025-12-08 00:00:47 +0300

    Skill button fix
    I fixed the skill button and added canvas back to level1.

--- DrakeShot-main/Drake Shot/Assets/scripts/UI/SkillButtonScript.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/UI/SkillButtonScript.cs
@@ -4,22 +4,30 @@ using UnityEngine.UI;
 
 public class SkillButtonScript : MonoBehaviour {
     
-    public PlayerScript myPlayer;
     public TextMeshProUGUI addText;
     public int skillCost;
     public string skillType;
     public float effectValue;
+    private PlayerScript _myPlayer;
+    private Button _myButton;
 
     private void Start() {
         
         addText.text = addText.text + "\nCost: " + skillCost + "\nValue: " + effectValue;
         
+        _myPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+        _myButton = GetComponent<Button>();
+
     }
     
     public void UnlockSkill() {
         
-        if (myPlayer.TryAndApplySkillEffect(skillCost, skillType, effectValue)) {
-            GetComponent<Button>().interactable = false;
+        if (_myPlayer == null) {
+            return;
+        }
+        
+        if (_myPlayer.TryAndApplySkillEffect(skillCost, skillType, effectValue)) {
+            _myButton.interactable = false;
         }
         
     }
