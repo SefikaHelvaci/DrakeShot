c24fa75e70543b2a6a33060d16d305a200e68a60
Mustafa Eren Şahin <mustafa.eren.sahin@outlook.com>
2025-12-09 16:57:41 +0300

    Some small changes

--- DrakeShot-main/Drake Shot/Assets/scripts/SceneTeleporterScript.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/SceneTeleporterScript.cs
@@ -17,7 +17,7 @@ public class SceneTeleporterScript : MonoBehaviour {
     
     private void OnTriggerStay2D(Collider2D other) {
         
-        if (other.CompareTag("Player") || Input.GetKeyDown(MenuScript.InteractionKey)) {
+        if (other.CompareTag("Player") && Input.GetKeyDown(MenuScript.InteractionKey)) {
             SceneManager.LoadScene(sceneToGo);
             
             other.transform.position = positionToGo;
