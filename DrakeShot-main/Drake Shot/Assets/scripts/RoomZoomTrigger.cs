2301dabe53176e9514b1128e016845066e001a69
SefikaH <hmmmakessense@gmail.com>
2025-12-16 12:46:15 +0300

    fixed the zoom
    the zoom trigger currently stays in the middle of the room, and you have to touch it manually. will place it in the spawn area when we do that.

--- DrakeShot-main/Drake Shot/Assets/scripts/RoomZoomTrigger.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/RoomZoomTrigger.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using UnityEngine;
-
+using Unity.Cinemachine;
 
 public class RoomZoomTrigger : MonoBehaviour
 {
-    public Camera targetCamera; // drag Main Camera
+    public CinemachineCamera cinemachineCamera;
     public float zoomedOutSize = 9f;
     public float zoomSpeed = 2f;
 
@@ -12,14 +12,14 @@ public class RoomZoomTrigger : MonoBehaviour
 
     void Awake()
     {
-        if (targetCamera == null)
-            targetCamera = Camera.main;
+        if (cinemachineCamera == null)
+            cinemachineCamera = FindObjectOfType<CinemachineCamera>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (triggered) return;
         if (!other.CompareTag("Player")) return;
+        if (triggered) return;
 
         triggered = true;
         StartCoroutine(ZoomOut());
@@ -27,14 +27,16 @@ public class RoomZoomTrigger : MonoBehaviour
 
     IEnumerator ZoomOut()
     {
-        float startSize = targetCamera.orthographicSize;
+        float startSize = cinemachineCamera.Lens.OrthographicSize;
         float t = 0f;
 
         while (t < 1f)
         {
             t += Time.deltaTime * zoomSpeed;
-            targetCamera.orthographicSize =
+
+            cinemachineCamera.Lens.OrthographicSize =
                 Mathf.Lerp(startSize, zoomedOutSize, t);
+
             yield return null;
         }
     }
