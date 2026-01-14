e920dd84e8ab18b26bee1afbb8efa8e2912fef9d
Mustafa Eren Şahin <mustafa.eren.sahin@outlook.com>
2026-01-14 21:11:44 +0300

    Cinemachine bug fix
    I fixed the cinemachine bug that happens when you enter level2.

--- DrakeShot-main/Drake Shot/Assets/scripts/RoomZoomTrigger.cs
+++ DrakeShot-main/Drake Shot/Assets/scripts/RoomZoomTrigger.cs
@@ -4,37 +4,38 @@ using Unity.Cinemachine;
 
 public class RoomZoomTrigger : MonoBehaviour
 {
-    public CinemachineCamera cinemachineCamera;
-    public float zoomedOutSize = 9f;
-    public float zoomSpeed = 2f;
+    
+    [SerializeField] private float zoomedOutSize = 9f;
+    [SerializeField] private float zoomSpeed = 2f;
 
-    bool triggered;
+    private CinemachineCamera _cinemachineCamera;
+    private bool _triggered;
 
-    void Awake()
-    {
-        if (cinemachineCamera == null)
-            cinemachineCamera = FindObjectOfType<CinemachineCamera>();
+    private void Start() {
+        
+        _cinemachineCamera = FindFirstObjectByType<CinemachineCamera>();
+        
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        if (triggered) return;
+        if (_triggered) return;
 
-        triggered = true;
+        _triggered = true;
         StartCoroutine(ZoomOut());
     }
 
     IEnumerator ZoomOut()
     {
-        float startSize = cinemachineCamera.Lens.OrthographicSize;
+        float startSize = _cinemachineCamera.Lens.OrthographicSize;
         float t = 0f;
 
         while (t < 1f)
         {
             t += Time.deltaTime * zoomSpeed;
 
-            cinemachineCamera.Lens.OrthographicSize =
+            _cinemachineCamera.Lens.OrthographicSize =
                 Mathf.Lerp(startSize, zoomedOutSize, t);
 
             yield return null;
